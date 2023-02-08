3814a58269551225e3a2942d26822688f874d213
Salah Eddine Ait Daoud <salaheddineaitdaoud@gmail.com>
2023-02-08 13:02:17 -0600

    finished W3D5

--- W3D5/q3_4/index.js
+++ W3D5/q3_4/index.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const path = require("path");
 const app= express();
+const cookieParser=require('cookie-parser');
 app.set("view engine",'ejs');
+app.use(express.urlencoded({extended:false}));
 
 let data = require("./public/database.js");
+var products_added=[];
+app.post("/addToCart",(req,res)=>{
+    const id=req.body.id;
+    products_added.push(data[id]);
+    res.redirect("/addToCart");
+})
+app.get("/addToCart",(req,res)=>{
+
+    res.render("myProducts",{products_added});
+})
 app.use("/",(req,res)=>{
-    // for(let element)
-    console.log(data.item1.name);
     res.render("index",{data});
 })
 app.listen(3000);
